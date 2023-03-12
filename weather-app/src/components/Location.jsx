24a1639a0948b77eae85be385b96b8ff50217035
Brigi <pap.mbrigitta@gmail.com>
2023-03-12 13:50:13 +0200

    implemented components

--- weather-app/src/components/Location.jsx
+++ weather-app/src/components/Location.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import axios from 'axios'
+import WeatherInfo from "./WeatherInfo";
 
 function Location() {
 
     const [location, setLocation] = useState('');  
     const [data, setData] = useState({});
-    
+
     //TODO: use dotenv for api key
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=3401a98f67244a2761343403f0115876`
 
@@ -15,20 +16,24 @@ function Location() {
             setData(response.data)
             console.log(response.data)
           })
+        
           setLocation('')
         }
       }
+      return (
+        <div className="app">
+            <div className="search">
+                <input
+                value={location}
+                onChange={event => setLocation(event.target.value)}
+                onKeyPress={searchLocation}
+                placeholder='Enter Location'
+                type="text" />
 
-    return (
-        <div className="search">
-            <input
-            value={location}
-             onChange={event => setLocation(event.target.value)}
-             onKeyPress={searchLocation}
-            placeholder='Enter Location'
-            type="text" />
-        </div>
-    );
+                <WeatherInfo weatherData={data} />
+            </div>
+          </div>
+        );
 }
 
 export default Location;
--- weather-app/src/components/WeatherInfo.jsx
+++ weather-app/src/components/WeatherInfo.jsx
@@ -1,12 +1,64 @@
 
+function WeatherInfo({weatherData}) {
 
-function WeatherInfo() {
-    return (
-        <div>
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+    
+        return `${day} ${date} ${month} ${year}`
+      }
 
+    if(Object.keys(weatherData).length != 0){
+        return (
+        <div className="container">
+            <div className="top">
+                <div className="location">
+                    <p>{weatherData.name}, {weatherData.sys.country}</p>
+                </div>
+                <div className="date">  
+                    {dateBuilder(new Date())}
+                </div>
+                <div className="temp">
+                    {weatherData.main ? <h1>{weatherData.main.temp.toFixed()}°C</h1> : null}
+                </div>
+                <div className="description"> 
+                    {weatherData.weather ? <p>{weatherData.weather[0].description}</p> : null}
+                </div>
+                <div className="sunrise">
+                    Sunrise: {new Date(weatherData.sys.sunrise * 1000 + weatherData.timezone).toUTCString().slice(-11,-4)} AM
+                </div>
+                <div className="sunset">
+                    Sunset: {new Date(weatherData.sys.sunset * 1000 + weatherData.timezone).toUTCString().slice(-11,-4)} PM
+                </div>
+            </div>
+
+            <div className="bottom">
+                    <div className="feels">
+                        {weatherData.main ? <p className='bold'>{weatherData.main.feels_like.toFixed()}°F</p> : null}
+                        <p>Feels Like</p>
+                    </div>
+                    <div className="humidity">
+                        {weatherData.main ? <p className='bold'>{weatherData.main.humidity}%</p> : null}
+                        <p>Humidity</p>
+                    </div>
+                    <div className="wind">
+                        {weatherData.wind ? <p className='bold'>{weatherData.wind.speed.toFixed()} MPH</p> : null}
+                        <p>Wind Speed</p>
+                    </div>
+                    <div className="wind">
+                        {weatherData.clouds.all ? <p className='bold'>{weatherData.clouds.all} %</p> : 'No clouds today'}
+                        <p>Cloudiness</p>
+                    </div>
+            </div> 
         </div>
 
-    );
+        );
+    }    
 }
 
 export default WeatherInfo;
